5c05fa71fad4585ed8682603dbd2cf0e69c063c5
Aya yassien <yassienaya714@gmail.com>
2025-07-17 16:56:51 +0300

    using an api to fetch todos array

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoForm } from "./components/TodoForm/TodoForm";
 import styles from "./App.module.css";
 import { TodoList } from "./components/TodoList/TodoList";
 import { TodoFilters } from "./components/TodoFilters/TodoFilters";
 
-const TODOS_DEFAULT = [
-  {
-    id: "1",
-    name: "Buy an Ice Cream",
-    description: "The white one with chocolate",
-    deadline: "2025-02-09",
-    priority: "low",
-    completed: false,
-  },
-  {
-    id: "2",
-    name: "Sell old MacBook Pro 2025",
-    description: "Try to sell it on OLX",
-    deadline: "2025-02-28",
-    priority: "high",
-    completed: false,
-  },
-  {
-    id: "3",
-    name: "Charge Powerbank",
-    description: "For the next travelling",
-    deadline: "2025-02-15",
-    priority: "medium",
-    completed: true,
-  },
-  {
-    id: "4",
-    name: "Test Todo only with a name",
-    description: "",
-    deadline: "",
-    priority: "none",
-    completed: false,
-  },
-];
-
 function App() {
-  const [todos, setTodos] = useState(TODOS_DEFAULT);
-  const [filters, setFilters] = useState({completed:"",priority:""});
+  const [todos, setTodos] = useState([]);
+  const [filters, setFilters] = useState({ completed: "", priority: "" });
+
+  function fetchTodos() {
+    fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}/todos`, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => !!res.ok && res.json())
+      .then(setTodos);
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
   function handleCreate(newTodo) {
     setTodos((prevTodos) => [
@@ -50,7 +28,7 @@ function App() {
     ]);
   }
 
-  function handleUpdate(id, newTodo){
+  function handleUpdate(id, newTodo) {
     setTodos((prevTodos) => prevTodos.map((todo) => todo.id === id ? newTodo : todo));
   }
 
@@ -76,11 +54,11 @@ function App() {
 
       <div className={styles.AppContainer}>
         <TodoForm onCreate={handleCreate} />
-        <TodoFilters onFilter={setFilters}/>
-        <TodoList 
-        todos={todos.filter(filterTodos)}
-        onUpdate={handleUpdate} 
-        onDelete={handleDelete}/>
+        <TodoFilters onFilter={setFilters} />
+        <TodoList
+          todos={todos.filter(filterTodos)}
+          onUpdate={handleUpdate}
+          onDelete={handleDelete} />
       </div>
     </div>
   );
--- src/schemas/todo.js
+++ src/schemas/todo.js
@@ -1,29 +1,29 @@
 import * as Yup from 'yup';
 import { PRIORITIES } from '../constants/priorities';
 
-export function getTodoSchema({ isNew = false }={}){
+export function getTodoSchema({ isNew = false } = {}) {
 
   const deadlineRule = Yup.string()
     .matches(/^\d{4}-\d{2}-\d{2}$/,
       "Deadline should be valid date in YYYY-MM-DD format")
     .nullable()
-    .transform((value, originalValue) => 
+    .transform((value, originalValue) =>
       originalValue === "" ? null : value);
 
   return Yup.object().shape({
     name: Yup.string()
       .required("Name is required")
-      .min(3,"Name should have min length of 3")
-      .max(50,"Name should have max length of 50"),
+      .min(3, "Name should have min length of 3")
+      .max(50, "Name should have max length of 50"),
     description: Yup.string()
       .max(200, "Description should have max length of 200"),
     deadline: isNew ? deadlineRule.test
-    ("is-future-date" ,"Deadline can't be in the past" ,
-    (value) => {
-      const today = new Date().toISOString().split("T")[0]
-      return value ? value >= today : true;
-    }
-  ) : deadlineRule,
+      ("is-future-date", "Deadline can't be in the past",
+        (value) => {
+          const today = new Date().toISOString().split("T")[0]
+          return value ? value >= today : true;
+        }
+      ) : deadlineRule,
     priority: Yup.string().required("Priority is not valid value")
       .oneOf(Object.keys(PRIORITIES), "Priority is not valid value"),
   });
